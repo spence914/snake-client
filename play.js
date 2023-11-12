c3face44b8609ce119c67c14e0493c2abc54e931
Spencer Lewis <spence.914@gmail.com>
2023-11-12 15:29:42 -0800

    add setUpInput function to listen for input from the client side, and implement a way to exit the program using CTRL^C

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
 const {connect} = require('./client');
 
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
