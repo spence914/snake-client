dde5c99adc3e1da49b477ee5c8645741c1acedb8
Spencer Lewis <spence.914@gmail.com>
2023-11-12 16:36:06 -0800

    assign movement keys to constant variables in constants.js then import them into input.js

--- input.js
+++ input.js
@@ -1,18 +1,23 @@
+const {MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY} = require('./constants');
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
   if (key === 'w') {
-    connection.write("Move: up");
+    connection.write(MOVE_UP_KEY);
   }
   if (key === 'a') {
-    connection.write("Move: left");
+    connection.write(MOVE_LEFT_KEY);
   }
   if (key === 's') {
-    connection.write("Move: down");
+    connection.write(MOVE_DOWN_KEY);
   }
   if (key === 'd') {
-    connection.write("Move: right");
+    connection.write(MOVE_RIGHT_KEY);
   }
 
   if (key === 'o') {
