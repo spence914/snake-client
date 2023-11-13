d5786312d1ae774c8d614d02375dee7ede5b9ab4
Spencer Lewis <spence.914@gmail.com>
2023-11-12 16:33:55 -0800

    create constants file to store frequently used constants

---
+++ constants.js
@@ -0,0 +1,15 @@
+const IP = 'localhost';
+const PORT = 50541;
+const MOVE_UP_KEY = "Move: up";
+const MOVE_DOWN_KEY = "Move: down";
+const MOVE_LEFT_KEY = "Move: left";
+const MOVE_RIGHT_KEY = "Move: right";
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY
+};
