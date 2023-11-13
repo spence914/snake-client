5cba56e0878306bdb7129cdc0627cbc7920b220b
Spencer Lewis <spence.914@gmail.com>
2023-11-12 16:03:42 -0800

    create connection object returned by connect() function, passed to setupInput function to allow data to be passed from the client side input to the server side event handler, allowing movement commands to be passed and executed

--- input.js
+++ input.js
@@ -2,10 +2,24 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
 };
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+let connection;
+const setupInput =  (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
