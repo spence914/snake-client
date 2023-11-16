53eddb5f96f689a1dddd43316b50f2cb2f5e98b1
Spencer Lewis <spence.914@gmail.com>
2023-11-16 12:07:07 -0800

    add comments to explain how constants are used to map input keys

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const IP = 'localhost';
 const PORT = 50541;
-const MOVE_UP_KEY = "w";
+const MOVE_UP_KEY = "w"; // Movement and message keys can easily be remapped here
 const MOVE_DOWN_KEY = "s";
 const MOVE_LEFT_KEY = "a";
 const MOVE_RIGHT_KEY = "d";
